26c7a851302af0ad1a4f2cd82a17f4fd070c8d66
Scott Frazer <scott.frazer@iongroup.com>
2025-03-18 20:18:04 -0400

    update secrets

--- taskdef/main.tf
+++ taskdef/main.tf
@@ -10,8 +10,7 @@ locals {
 }
 
 module "task_definition" {
-  source                = "mergermarket/ecs-task-definition/acuris"
-  version               = "2.3.0"
+  source                = "./task_definition"
   family                = var.family
   container_definitions = var.container_definitions
   task_role_arn         = aws_iam_role.task_role.arn
---
+++ taskdef/task_definition/variables.tf
@@ -0,0 +1,54 @@
+variable "family" {
+  description = "A unique name for your task defintion."
+  type        = string
+}
+
+variable "container_definitions" {
+  description = "A list of valid container definitions provided as a single valid JSON document."
+  type        = list(string)
+}
+
+variable "task_role_arn" {
+  description = "The Amazon Resource Name for an IAM role for the task"
+  type        = string
+  default     = ""
+}
+
+variable "execution_role_arn" {
+  description = "The Amazon Resource Name for an execution role for the task"
+  type        = string
+  default     = ""
+}
+
+variable "volume" {
+  description = "Volume block map with 'name' and 'host_path'."
+  type        = map(string)
+  default     = {}
+}
+
+variable "network_mode" {
+  description = "The Docker networking mode to use for the containers in the task"
+  type        = string
+  default     = "bridge"
+}
+
+variable "volumes" {
+  type = map(object({
+    name = string
+    host_path = string
+  }))
+  default = {
+  }
+}
+
+variable "placement_constraint_on_demand_only" {
+  description = "Add placement constraint to only run on on-demand instances"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource."
+  type        = map(string)
+  default = {}
+}
